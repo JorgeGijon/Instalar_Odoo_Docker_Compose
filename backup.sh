5a893884b863707bf3739aa61a86826da2e70afb
JorgeGR <jorgegr.gijon@gmail.com>
2023-11-03 15:52:59 +0100

    v6

--- backup.sh
+++ backup.sh
@@ -2,7 +2,6 @@
 ID_CONTENEDOR=$1
 
 # Configuración de la base de datos PostgreSQL
-PG_HOST=db                                          #nombre_del_contenedor_postgres
 PG_DATABASE=postgres                                #nombre_de_la_base_de_datos
 PG_USER=odoo                                        #nombre_de_usuario
 PG_PATH=/home/ubuntu/copias_seguridad               #ruta local donde colocar carpeta backups
@@ -14,5 +13,5 @@ BACKUP_FILE=backup_$(date +"%Y-%m-%d_%H-%M-%S").sql
 #sudo docker ps
 sudo mkdir /home/ubuntu/copias_seguridad
 sudo chmod 777 /home/ubuntu/copias_seguridad
-sudo docker exec -it e897 pg_dump -U $PG_USER $PG_DATABASE > $PG_PATH/$BACKUP_FILE
+sudo docker exec -it e897 pg_dump -U odoo postgres > /home/ubuntu/copias_seguridad/backup_$(date +"%Y-%m-%d_%H-%M-%S").sql
 sudo find /home/ubuntu/copias_seguridad -type f -name "backup_*.sql" -mtime +7 -exec rm {} \;
