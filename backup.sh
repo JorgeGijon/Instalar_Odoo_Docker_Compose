dc15b2f2aea27bb6c08b6fffaba8e40256f7830d
JorgeGR <jorgegr.gijon@gmail.com>
2023-11-03 16:54:20 +0100

    v6

--- backup.sh
+++ backup.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-#ID_CONTENEDOR=$1
+ID_CONTENEDOR=$1        #nombre contenedor o id del docker postgresql (sirven 4 dig)
 
-# Configuración de la base de datos PostgreSQL
+# Variables
 PG_DATABASE=postgres                                #nombre_de_la_base_de_datos
 PG_USER=odoo                                        #nombre_de_usuario
 PG_PATH=/home/ubuntu/copias_seguridad               #ruta local donde colocar carpeta backups
-ID_CONTENEDOR=e897                                  #nombre contenedor o id del docker postgresql (sirven 4 dig)
 BACKUP_FILE=backup_$(date +"%Y-%m-%d_%H-%M-%S").sql
 
-# Realizar la copia de seguridad utilizando pg_dump
-#sudo docker exec -it e897 /bin/bash
-#sudo docker ps
+# Comprobar carpeta backups y permisos
 if [ ! -d $PG_PATH ]; then
     # Si no existe, crea el directorio
     sudo mkdir -p  $PG_PATH
@@ -20,8 +17,8 @@ else
     # Si ya existe, muestra un mensaje
     echo "El directorio "$PG_PATH" ya existe."
 fi
-#sudo mkdir /home/ubuntu/copias_seguridad
-#sudo chmod 777 /home/ubuntu/copias_seguridad
+# Realizar la copia de seguridad utilizando pg_dump
 sudo docker exec -t $ID_CONTENEDOR pg_dump -U $PG_USER $PG_DATABASE > $PG_PATH/$BACKUP_FILE
+# Borrar Backup de + 7 dias
 sudo find $PG_PATH -type f -name "backup_*.sql" -mtime +7 -exec rm {} \;
 echo "Copia de seguridad realizada: "$BACKUP_FILE
--- documents/copia_seguridad_postgres.md
+++ documents/copia_seguridad_postgres.md
@@ -4,18 +4,21 @@ sudo apt install postgresql-client-common
 pg_dump --version
 
 
-1. Ejecutar fichero backup.sh
+1. Ejecutar siguiente `curl` para configurar la copia de la Base Datos. Argumentos por defecto para reemplazar:
+* **e897**: id contendor de postgresql `sudo docker ps`
 ``` bash
-curl -s https://raw.githubusercontent.com/JorgeGijon/Instalar_Odoo_Docker_Compose/main/backup.sh | sudo bash e897
+curl -s https://raw.githubusercontent.com/JorgeGijon/Instalar_Odoo_Docker_Compose/main/backup.sh | sudo bash -s e897
 ```
 
-2. Hacer el Script Ejecutable:
+2. Crear fichero en `/home/ubuntu/odoo-01/backup.sh`  (copiar contenido fichero [backup.sh](backup.sh))
+
+3. Hacer el Script Ejecutable:
 ``` bash
 chmod +x backup.sh
 ``` 
 
-3. Configurar un Cronjob:
+4. Configurar un Cronjob:
 Usa crontab -e para abrir el editor de crontab y programa el script para que se ejecute según la frecuencia que desees. Por ejemplo, para ejecutar la copia de seguridad todos los días a las 2 AM, agrega la siguiente línea:
 ``` bash
-0 2 * * * /ruta/completa/del/backup.sh
+0 2 * * * /home/ubuntu/odoo-01/backup.sh
 ```
