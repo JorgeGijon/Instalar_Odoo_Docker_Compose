767c89fddf5344ed0d401f6a7ce8a2a5ab4b4a91
JorgeGR <61075878+JorgeGijon@users.noreply.github.com>
2025-01-21 10:00:27 +0100

    Create run_v17.sh

---
+++ run_v17.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+DESTINATION=$1
+PORT=$2
+CHAT=$3
+MASTERPASSWORD=$4
+# clonar directorio Odoo
+git clone --depth=1 https://github.com/JorgeGijon/Instalar_Odoo_Docker_Compose $DESTINATION
+rm -rf $DESTINATION/.git
+# set permission
+mkdir -p $DESTINATION/postgresql
+sudo chmod -R 777 $DESTINATION
+
+# config
+if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
+sudo sysctl -p
+sed -i 's/10001/'$PORT'/g' $DESTINATION/docker-compose_v17.yml
+sed -i 's/20001/'$CHAT'/g' $DESTINATION/docker-compose_v17.yml
+# correr Odoo
+docker-compose -f $DESTINATION/docker-compose_v17.yml up -d
+
+echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: '$MASTERPASSWORD' | Live chat port: '$CHAT
+
+# cambiar master password por defecto
+sudo sed -i 's/pass.por.defecto/'$MASTERPASSWORD'/g' "/home/ubuntu/"$DESTINATION"/etc/odoo.conf"
